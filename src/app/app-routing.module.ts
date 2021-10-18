67f602c7f00322bc821d671c5a267e3707c9cee9
Ali Raza <dejavu87@gmail.com>
2021-10-19 03:43:01 +0500

    added shortening component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShortenComponent } from './shorten/shorten.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: ShortenComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shorten/shorten.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shorten',
+  templateUrl: './shorten.component.html',
+  styleUrls: ['./shorten.component.css']
+})
+export class ShortenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
